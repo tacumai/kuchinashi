1de4ebfbdd3bc0c9684d62775e76b2c078282312
tacmac <tacuma.igei@gmail.com>
2017-05-14 21:43:57 +0900

    add player class

--- src/js/game.js
+++ src/js/game.js
@@ -1,13 +1,15 @@
+const Player = require('./player.js');
+
 class Game {
   constructor(player1, player2) {
-    this.player1 = player1;
-    this.player2 = player2;
+    this.black = new Player(player1);
+    this.white = new Player(player2);
   }
 
   startGame() {
     console.log('ゲームを開始します.');
-    console.log(`先手は${this.player1}です。`);
-    console.log(`後手は${this.player2}です。`);
+    console.log(this.black.name);
+    console.log(this.white.name);
   }
 
   stopGame() {
